73105ee09df2e7bdeb051cf261beed8c4a16ed91
Jon Coello <joncoello@gmail.com>
2017-03-10 10:07:32 +0000

    get sass building using gulp

---
+++ AccessStyleGuideExample/gulpfile.js
@@ -0,0 +1,19 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var sass = require("gulp-sass");
+var paths = {
+    webroot: "./"
+}
+paths.scss = paths.webroot + "styles/**/*.scss";
+gulp.task('sass', function () {
+    gulp.src(paths.scss)
+      .pipe(sass())
+      .pipe(gulp.dest(paths.webroot + "css"));
+});
+gulp.task('watch-sass', function () {
+    gulp.watch(paths.scss, ['sass']);
+})
